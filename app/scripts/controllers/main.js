bb0ec01ca25b5b44aa2fc96b30432bd0e74de169
Patricio Rojas <brujo@Patricios-MacBook-Pro.local>
2015-04-27 21:02:05 -0400

    first commit, yeoman angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularChartApp
+ */
+angular.module('angularChartApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
